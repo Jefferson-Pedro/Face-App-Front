a55fff4b20476802887539181f837aa7e8be3726
Jefferson Pedro <jeffersonpedro05@gmail.com>
2026-01-02 11:03:37 -0300

    ajustando o enquadramento do rosto

--- Face-App/src/app/features/camera-capture/camera-capture/camera-capture.component.ts
+++ Face-App/src/app/features/camera-capture/camera-capture/camera-capture.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-camera-capture',
@@ -6,6 +6,52 @@ import { Component } from '@angular/core';
   templateUrl: './camera-capture.component.html',
   styleUrl: './camera-capture.component.scss'
 })
-export class CameraCaptureComponent {
+export class CameraCaptureComponent implements OnInit {
+
+
+  @ViewChild('video', { static: true })
+  videoElement!: ElementRef<HTMLVideoElement>;
+
+  @ViewChild('canvas', { static: true })
+  canvasElement!: ElementRef<HTMLCanvasElement>;
+
+  ngOnInit(): void {
+    this.initCam();
+  }
+
+  public initCam(){
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then((stream) => {
+        const video = this.videoElement?.nativeElement;
+        if (video) {
+          video.srcObject = stream;
+          video.play();
+        }
+
+      })
+      .catch((error) => {
+        console.error('Não foi possivel acessar a câmera: ', error);
+      });
+  }
+
+  public takePicture(){
+    const video = this.videoElement?.nativeElement;
+    const canvas = this.canvasElement?.nativeElement;
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    const context = canvas.getContext('2d');
+    if (context) {
+      context.drawImage(video, 0, 0, canvas.width, canvas.height);
+      const urlImage = canvas.toDataURL('image/png');
+
+      const linkDonwload = document.createElement('a');
+      linkDonwload.href = urlImage;
+      linkDonwload.download = 'foto.png';
+      linkDonwload.click();
+
+      console.log(urlImage);
+      console.log('Foto tirada com sucesso!');
+    }
+  }
 
 }
