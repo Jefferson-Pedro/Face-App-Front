928847d40a55f7b631c8b48465545b9eba93f097
Jefferson Pedro <jeffersonpedro05@gmail.com>
2026-01-02 17:24:08 -0300

    Preparando os serviços para login de usuários

--- Face-App/src/app/shared/services/auth-service/auth-service.service.ts
+++ Face-App/src/app/shared/services/auth-service/auth-service.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment.development';
+import { UserLoginRequest } from '../../../core/models/UserLoginRequest';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthServiceService {
+export class AuthService {
 
-  constructor() { }
+  private route = inject(Router);
+  private http = inject(HttpClient);
+
+  public login(login: UserLoginRequest) {
+    const url = `${environment.baseUrl}/users/login`;
+
+    return this.http.post(url, login);
+  }
 }
