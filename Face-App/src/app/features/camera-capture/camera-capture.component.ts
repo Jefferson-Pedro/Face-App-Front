f29713f24d6f52f5f01841ed72f0cf6262169f52
Jefferson Pedro <jeffersonpedro05@gmail.com>
2026-01-04 22:44:02 -0300

    tratamento da imagem e criando um subscribe para envio da mesma para o back

--- Face-App/src/app/features/camera-capture/camera-capture.component.ts
+++ Face-App/src/app/features/camera-capture/camera-capture.component.ts
@@ -1,4 +1,6 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, inject, OnInit, ViewChild } from '@angular/core';
+import { FaceDTO } from '../../core/models/FaceRequest';
+import { FaceService } from '../../shared/services/face-service/face-service.service';
 
 @Component({
   selector: 'app-camera-capture',
@@ -14,6 +16,10 @@ export class CameraCaptureComponent implements OnInit {
 
   @ViewChild('canvas', { static: true })
   canvasElement!: ElementRef<HTMLCanvasElement>;
+
+  private faceService = inject(FaceService);
+
+  private face: FaceDTO | undefined;
 
   ngOnInit(): void {
     this.initCam();
@@ -44,12 +50,30 @@ export class CameraCaptureComponent implements OnInit {
       context.drawImage(video, 0, 0, canvas.width, canvas.height);
       const urlImage = canvas.toDataURL('image/png');
 
+      // Remove o prefixo "data:image/png;base64,"
+      const base64Data = urlImage.split(',')[1];
+
+      // Preenche o objeto face
+      this.face = {
+        type: 'image/png',
+        data: base64Data
+      };
+
+      this.faceService.registerFace(this.face).subscribe({
+        next: (response) => {
+          console.log('Face registrada com sucesso:', response);
+        },
+        error: (error) => {
+          console.error('Erro ao registrar face:', error);
+        }
+      });
+
       const linkDonwload = document.createElement('a');
       linkDonwload.href = urlImage;
       linkDonwload.download = 'foto.png';
       linkDonwload.click();
 
-      console.log(urlImage);
+      console.log('Face capturada:', this.face);
       console.log('Foto tirada com sucesso!');
     }
   }
